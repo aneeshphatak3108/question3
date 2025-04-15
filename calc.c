a9769886c2cd2d4537e27f006380d41ef4886304
= <=>
2025-04-15 11:35:10 +0530

    Added Power Operator

---
+++ calc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>  // Added for pow()
+
+int main() {
+    char op;
+    double num1, num2;
+
+    printf("Enter an operator (+, -, *, /, ^): ");
+    scanf(" %c", &op);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (op) {
+        case '+': printf("%.2lf\n", num1 + num2); break;
+        case '-': printf("%.2lf\n", num1 - num2); break;
+        case '*': printf("%.2lf\n", num1 * num2); break;
+        case '/': 
+            if (num2 != 0)
+                printf("%.2lf\n", num1 / num2);
+            else
+                printf("Division by zero error.\n");
+            break;
+        case '^': printf("%.2lf\n", pow(num1, num2)); break;
+        default:
+            printf("Invalid operator.\n");
+    }
+
+    return 0;
+}
+
